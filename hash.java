539ebfd5e82fba58c004330f35eb4fc9bf00e4f7
Timur-Safin <tim220617@gmail.com>
2025-09-28 16:28:26 +0300

    hash.java

--- hash.java
+++ hash.java
@@ -1,1 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
 
+public class DrinksMap {
+    public static void main(String[] args) {
+        // создаём словарь (HashMap) где ключ - String, значение - Integer
+        Map<String, Integer> hash = new HashMap<>();
+
+        hash.put("beer", 100);
+        hash.put("wisky", 650);
+        hash.put("vodka", 350);
+
+        // выводим содержимое
+        for (Map.Entry<String, Integer> entry : hash.entrySet()) {
+            System.out.println(entry.getKey() + ", " + entry.getValue());
+        }
+    }
+}
