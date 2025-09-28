1ab355742f6381190b461f8bb61bfc41f831c040
Timur-Safin <tim220617@gmail.com>
2025-09-28 16:12:36 +0300

    hash.cpp

--- hash.cpp
+++ hash.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <map>
+#include <string>
 
+int main() {
+    // создаём словарь (map) где ключ - string, значение - int
+    std::map<std::string, int> hash;
+
+    hash["beer"] = 100;
+    hash["wisky"] = 650;
+    hash["vodka"] = 350;
+
+    // выводим содержимое
+    for (const auto& pair : hash) {
+        std::cout << pair.first << ", " << pair.second << std::endl;
+    }
+
+    return 0;
+}
